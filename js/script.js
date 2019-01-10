755e5e6f25b21069cb60fcee495ce2d68bab0f7f
Aleksandra Lipert <ola.luszcz@interia.pl>
2019-01-10 01:38:14 +0100

    Change script.js

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ function Button(text) {
 Button.prototype = {
     create: function() {
         var self = this;
-        this.element = window.document.createElement('button');
+        this.element = document.createElement('button');
         this.element.innerText = this.text;
         this.element.addEventListener('click', function() {
             alert(self.text);
